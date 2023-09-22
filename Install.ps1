52a3b49006a1bc66cc45b01ce21b20e006075556
Tony Nguyen <TNguyen@paritycorporation.onmicrosoft.com>
2023-09-22 16:34:04 +0700

    Update Install script

--- Install.ps1
+++ Install.ps1
@@ -38,20 +38,29 @@ Write-Verbose "ModuleDir: $ModuleDir"
 [Environment]::SetEnvironmentVariable('PowerLauncher_InstallDir', $InstallDir, $Scope)
 Write-Verbose "Write $InstallDir to `$env:PowerLauncher_InstallDir"
 
+$CurrentModulePaths = [Environment]::GetEnvironmentVariable('PSModulePath', $Scope) -split ';'
+Write-Verbose "PSModulePath: $CurrentModulePaths"
 
-$MainFilePath = "$InstallDir\Main.ps1"
-$ShortcutFilePath = "$InstallDir\Run.lnk"
+$ModuleTargetPath = $CurrentModulePaths[0]
+Copy-Item "$ModuleDir\PowerLogger" -Destination "$ModuleTargetPath\PowerLogger" -Recurse
+Write-Verbose "Copy module PowerLogger to $ModuleTargetPath"
+Copy-Item "$ModuleDir\PowerLauncher" -Destination "$ModuleTargetPath\PowerLauncher" -Recurse
+Write-Verbose "Copy module PowerLauncher to $ModuleTargetPath"
 
-$WshShell = New-Object -comObject WScript.Shell
-$Shortcut = $WshShell.CreateShortcut($ShortcutFilePath)
-$Shortcut.TargetPath = "powershell.exe"
-$Shortcut.Arguments = "-command ""& '$MainFilePath'"""
-$Shortcut.Save()
 
-Write-Verbose "Created shortcut $ShortcutFilePath for $MainFilePath"
-
-$bytes = [System.IO.File]::ReadAllBytes("$ShortcutFilePath")
-$bytes[0x15] = $bytes[0x15] -bor 0x20 #set byte 21 (0x15) bit 6 (0x20) ON
-[System.IO.File]::WriteAllBytes("$ShortcutFilePath", $bytes)
-
-Write-Verbose "Allow the shortcut ($ShortcutFilePath) run as Administrator"
+# $MainFilePath = "$InstallDir\Main.ps1"
+# $ShortcutFilePath = "$InstallDir\Run.lnk"
+# 
+# $WshShell = New-Object -comObject WScript.Shell
+# $Shortcut = $WshShell.CreateShortcut($ShortcutFilePath)
+# $Shortcut.TargetPath = "powershell.exe"
+# $Shortcut.Arguments = "-command ""& '$MainFilePath'"""
+# $Shortcut.Save()
+# 
+# Write-Verbose "Created shortcut $ShortcutFilePath for $MainFilePath"
+# 
+# $bytes = [System.IO.File]::ReadAllBytes("$ShortcutFilePath")
+# $bytes[0x15] = $bytes[0x15] -bor 0x20 #set byte 21 (0x15) bit 6 (0x20) ON
+# [System.IO.File]::WriteAllBytes("$ShortcutFilePath", $bytes)
+# 
+# Write-Verbose "Allow the shortcut ($ShortcutFilePath) run as Administrator"
