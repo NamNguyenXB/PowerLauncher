c4ee18f87606223265decf1ae5002f3eb5e2ad25
Tony Nguyen <TNguyen@paritycorporation.onmicrosoft.com>
2023-09-28 08:59:37 +0700

    minor update

--- Modules/PowerLauncher/public/Invoke-Launcher.ps1
+++ Modules/PowerLauncher/public/Invoke-Launcher.ps1
@@ -16,23 +16,25 @@ function Invoke-Launcher {
     $Launcher,
     $Config
   )
-  if ($Launcher.Run) {
-    Write-LauncherHead($Launcher)
-
-    $Type = $Launcher.Type
-    $FunctionName = "Invoke-" + $Type
-
-    if ($null -eq (Get-Command "$FunctionName" -EA SilentlyContinue)) {
-      Install-LauncherType -t $Type -d $Config.InstallFolder + "\launchers"
-    }
-
-    if ($null -ne (Get-Command "$FunctionName" -EA SilentlyContinue)) {
-      &"$FunctionName" $Launcher $Config $Verbose
-    }
-    else {
-      Write-Output "|  Cannot run! Type $Type is not supported."
-    }
-
-    Write-LauncherTail($Launcher)
+  if (($null -eq $Launcher) -or (-not $Launcher.Run)) {
+    return;
   }
+
+  Write-LauncherHead($Launcher)
+
+  $Type = $Launcher.Type
+  $FunctionName = "Invoke-" + $Type
+
+  if ($null -eq (Get-Command "$FunctionName" -EA SilentlyContinue)) {
+    Install-LauncherType -t $Type -d $Config.InstallFolder + "\launchers"
+  }
+
+  if ($null -ne (Get-Command "$FunctionName" -EA SilentlyContinue)) {
+    &"$FunctionName" $Launcher $Config
+  }
+  else {
+    Write-Content "|  Cannot run! Type $Type is not supported."
+  }
+
+  Write-LauncherTail($Launcher)
 }
--- Modules/PowerLogger/public/Write-Line.ps1
+++ Modules/PowerLogger/public/Write-Line.ps1
@@ -3,7 +3,10 @@
 Write a line.
 
 .PARAMETER LineLength
-Length of the line. Defaults to 120
+Length of the line. Defaults to 120.
+
+.PARAMETER Character
+Character to draw the line. Default to '-'.
 
 .EXAMPLE
 Write-Line
@@ -14,8 +17,19 @@ function Write-Line {
   param(
     [Alias("l")]
     [Parameter(HelpMessage = "Length of the line. Default to 120.")]
-    [int16] $LineLength = 120
+    [int16] $LineLength = 120,
+    [Alias("c")]
+    [Parameter(HelpMessage = "Character to draw the line. Default to '-'.")]
+    $Character = "-"
   )
-  $Line = "-" * $LineLength
+  if(($null -eq $Character) -or ($Character.Length -lt 1)){
+    $Character = "-"
+  }
+
+  if($Character.Length -gt 1){
+    $Character = $Character[0]
+  }
+
+  $Line = $Character * $LineLength
   Write-Content $Line
 }
