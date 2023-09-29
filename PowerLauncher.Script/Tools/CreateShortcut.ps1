57a0daa766acd7649c1e7261821f06bdfa3977f1
Tony Nguyen <TNguyen@paritycorporation.onmicrosoft.com>
2023-09-29 18:57:20 +0700

    Re-oganize folder tree

---
+++ PowerLauncher.Script/Tools/CreateShortcut.ps1
@@ -0,0 +1,109 @@
+﻿<#
+.SYNOPSIS
+Install PowerLauncher
+
+.PARAMETER Scope
+Install Scope.
+
+.PARAMETER InstallDir
+Install Directory (Optional)
+
+.EXAMPLE
+
+#>
+
+#requires -RunAsAdministrator
+
+param (
+  [Alias("s")]
+  $SourceDirectory,
+  [Alias("d")]
+  $DestinationDirectory,
+  [Alias("f")]
+  $FileName = "Main.ps1",
+  [Alias("n")]
+  $ShortcutName = "Run.lnk"
+)
+
+# Validation
+$IsValid = $true
+$lineLength = 120
+$blankLine = " " * $lineLength
+$dashLine = "-" * $lineLength
+$shortDashLine = "-" * ($lineLength / 2)
+
+
+#Write-Output "$dashLine"
+
+# Validate $SourceDirectory
+# $SourceDirectory is required.
+# Defaults to the directory in which this script locates.
+$origpos = $host.UI.RawUI.CursorPosition
+Write-Output "[CHK] Validating SourceDirectory..."
+IF ($null -eq $SourceDirectory) {
+  $ThisScriptDir = $PSScriptRoot
+  IF ( $null -eq $ThisScriptDir) {
+    $ThisScriptDir = Split-Path -Path ($MyInvocation.MyCommand.Path)
+  }
+  $SourceDirectory = $ThisScriptDir
+}
+
+IF ($null -eq $SourceDirectory) {
+  $IsValid = $false
+  throw "Validation Failed. `$SourceDirectory was not found. If you are remotely installing, Please specify -SourceDirectory parameter."
+}
+$host.UI.RawUI.CursorPosition = $origpos
+Write-Output $blankLine
+$host.UI.RawUI.CursorPosition = $origpos
+Write-Output "[CHK] SourceDirectory - Valid"
+
+# Validate $DestinationDirectory
+$origpos = $host.UI.RawUI.CursorPosition
+Write-Output "[CHK] Validating DestinationDirectory..."
+IF ( $null -eq $DestinationDirectory) {
+  $DestinationDirectory = $SourceDirectory
+}
+$host.UI.RawUI.CursorPosition = $origpos
+Write-Output $blankLine
+$host.UI.RawUI.CursorPosition = $origpos
+Write-Output "[CHK] DestinationDirectory - Valid"
+
+
+IF ( -not $IsValid) {
+  throw 'Validation Failed.'
+}
+
+Write-Output "[CHK] Validation completed. No error found."
+#Write-Output $dashLine
+
+$origpos = $host.UI.RawUI.CursorPosition
+Write-Output "[INF] Creating the shortcut $ShortcutFilePath for $MainFilePath..."
+
+$MainFilePath = "$SourceDirectory\$FileName"
+$ShortcutFilePath = "$DestinationDirectory\$ShortcutName"
+ 
+$WshShell = New-Object -comObject WScript.Shell
+$Shortcut = $WshShell.CreateShortcut($ShortcutFilePath)
+$Shortcut.TargetPath = "powershell.exe"
+$Shortcut.Arguments = "-command ""& '$MainFilePath'"""
+$Shortcut.Save()
+
+$host.UI.RawUI.CursorPosition = $origpos
+Write-Output $blankLine
+$host.UI.RawUI.CursorPosition = $origpos
+Write-Output "[INF] Created shortcut $ShortcutFilePath for $MainFilePath"
+ 
+
+$origpos = $host.UI.RawUI.CursorPosition
+Write-Output "[INF] Modifying the shortcut ($ShortcutFilePath)..."
+$bytes = [System.IO.File]::ReadAllBytes("$ShortcutFilePath")
+$bytes[0x15] = $bytes[0x15] -bor 0x20 #set byte 21 (0x15) bit 6 (0x20) ON
+[System.IO.File]::WriteAllBytes("$ShortcutFilePath", $bytes)
+ 
+$host.UI.RawUI.CursorPosition = $origpos
+Write-Output $blankLine
+$host.UI.RawUI.CursorPosition = $origpos
+Write-Output "[INF] Allow the shortcut ($ShortcutFilePath) run as Administrator"
+Write-Output "[INF] Done! The shortcut was created successfully."
+
+#Write-Output $dashLine
---
+++ PowerLauncher.Script/Tools/InstallSubModule.ps1
@@ -0,0 +1,91 @@
+﻿<#
+.SYNOPSIS
+Install PowerLauncher
+
+.PARAMETER Scope
+Install Scope.
+
+.PARAMETER InstallDir
+Install Directory (Optional)
+
+.EXAMPLE
+
+#>
+
+#requires -RunAsAdministrator
+
+param (
+  $SourceDirectory,
+  $DestinationDirectory,
+  $TargetName = 'SubModule'
+)
+
+# Get Default InstallDir
+$CopyScripts = 1
+
+# Validation
+Write-Output "Validating ..."
+$IsValid = $true
+
+# Validate $SourceDirectory
+# $SourceDirectory is required.
+# Defaults to the directory in which this script locates.
+IF ($null -eq $SourceDirectory) {
+  $ThisScriptDir = $PSScriptRoot
+  IF ( $null -eq $ThisScriptDir) {
+    $ThisScriptDir = Split-Path -Path ($MyInvocation.MyCommand.Path)
+  }
+  $SourceDirectory = $ThisScriptDir
+}
+Write-Output "`$SourceDirectory=$SourceDirectory"
+IF ($null -eq $SourceDirectory) {
+  $IsValid = $false
+  throw "Validation Failed. `$SourceDirectory was not found. If you are remotely installing, Please specify -SourceDirectory parameter."
+}
+
+# Validate $DestinationDirectory
+IF ( $null -eq $DestinationDirectory) {
+  $DestinationDirectory = $SourceDirectory
+}
+Write-Output "`$DestinationDirectory=$DestinationDirectory"
+
+# Validate $TargetName
+IF (( $null -eq $TargetName) -or ("" -eq $TargetName)) {
+  throw "TargetName cannot be null or Empty"
+  $IsValid = $false
+}
+
+$TargetDirectory = $DestinationDirectory + "/" + $TargetName
+
+
+IF ( -not $IsValid) {
+  throw 'Validation Failed.'
+}
+
+IF ( -not $Force) {
+  Read-Host "Validations passed. Press Enter to continue."
+}
+else {
+  Write-Output "Validations passed."
+}
+
+Write-Output "Start installing..."
+
+IF ($CopyScripts -eq 1) {
+  Write-Output "Copy $SourceDirectory\Main.ps1 to $TargetDirectory"
+  Copy-Item "$SourceDirectory\Main.ps1" -Destination $TargetDirectory
+
+  Write-Output "Copy $SourceDirectory\launch.json to $TargetDirectory"
+  Copy-Item "$SourceDirectory\launch.json" -Destination $TargetDirectory
+
+  IF (-not (Test-Path $DestinationDirectory\launchers)) {
+    Write-Output "Create folder 'launchers'"
+    New-Item -ItemType Directory -Path $DestinationDirectory\launchers
+  }
+
+  Copy-Item "$SourceDirectory\launchers" -Destination $TargetDirectory -Recurse
+}
+
+
+
+Write-Output "Software has been installed successfully."
