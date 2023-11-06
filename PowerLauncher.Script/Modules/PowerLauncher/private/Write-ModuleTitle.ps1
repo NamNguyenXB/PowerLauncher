5a5e8915ba55089078684827db704a5574150e92
Tony Nguyen <TNguyen@paritycorporation.onmicrosoft.com>
2023-11-06 09:57:57 +0700

    Update code

--- PowerLauncher.Script/Modules/PowerLauncher/private/Write-ModuleTitle.ps1
+++ PowerLauncher.Script/Modules/PowerLauncher/private/Write-ModuleTitle.ps1
@@ -11,28 +11,55 @@ function Write-ModuleTitle{
   # Save the log level.
   $OriginalLevel = $LogLevel;
 
-  $BoderThick = 1
+  $BoderThick = 0
 
   $Border = " " * $BoderThick
-  $Prefix = " "
+  $Prefix_1 = " Module: "
   $ModuleID = $Module.ID
-  $Delimiter = ": "
+  $Postfix_1 = " "
+  $Delimiter = " "
+  $Prefix_2 = "( "
   $ModuleName = $Module.Name
-  $Postfix = " "
-  
-  $ModuleLength = ($Prefix.Length + $ModuleID.Length + $Delimiter.Length + $ModuleName.Length + $Postfix.Length  + $BoderThick * 2)
+  $Postfix_2 = " )"
+
+  $ModuleLength = $Prefix_1.Length + $ModuleID.Length + $Postfix_1.Length
+  $ModuleLength = $ModuleLength + $Delimiter.Length
+  $ModuleLength = $ModuleLength + $Prefix_2.Length + $ModuleName.Length + $Postfix_2.Length
+  $ModuleLength = $ModuleLength + $BoderThick * 2
 
   # Write a blue line
-  Write-Line -Size $ModuleLength -Content " " -Color Blue
+  Write-Line -Size $ModuleLength -Content " " -Color Black
 
   # Write Content
-  Write-Content -Content $Border -BackgroundColor Blue -NoNewline
-  Write-Content -Content "$Prefix" -BackgroundColor Yellow -ForegroundColor Black -NoNewline
-  Write-Content -Content "$ModuleID" -BackgroundColor Yellow -ForegroundColor Black -NoNewline
-  Write-Content -Content "$Delimiter" -BackgroundColor Yellow -ForegroundColor Black -NoNewline
-  Write-Content -Content "$ModuleName" -BackgroundColor Yellow -ForegroundColor Black -NoNewline
-  Write-Content -Content "$Postfix" -BackgroundColor Yellow -ForegroundColor Black -NoNewline
-  Write-Content -Content $Border -BackgroundColor Blue
+
+  # Left Boder
+  $BackgroundColor = 'Blue'
+  $ForegroundColor = 'White'
+  Write-Content -Content $Border -BackgroundColor $BackgroundColor -ForegroundColor $ForegroundColor -NoNewline
+
+  # Module ID: <Prefix_1><ModuleID><Postfix_1>
+  $BackgroundColor = 'Yellow'
+  $ForegroundColor = 'Black'
+  Write-Content -Content "$Prefix_1" -BackgroundColor $BackgroundColor -ForegroundColor $ForegroundColor -NoNewline
+  Write-Content -Content "$ModuleID " -BackgroundColor $BackgroundColor -ForegroundColor $ForegroundColor -NoNewline
+  Write-Content -Content "$Postfix_1" -BackgroundColor $BackgroundColor -ForegroundColor $ForegroundColor -NoNewline
+
+  # Delimiter
+  $BackgroundColor = 'Black'
+  $ForegroundColor = 'White'
+  Write-Content -Content "$Delimiter" -BackgroundColor $BackgroundColor -ForegroundColor $ForegroundColor -NoNewline
+
+  # Module name: <Prefix_2><ModuleName><Postfix_2>
+  $BackgroundColor = 'Black'
+  $ForegroundColor = 'White'
+  Write-Content -Content "$Prefix_2" -BackgroundColor $BackgroundColor -ForegroundColor $ForegroundColor -NoNewline
+  Write-Content -Content "$ModuleName" -BackgroundColor $BackgroundColor -ForegroundColor $ForegroundColor -NoNewline
+  Write-Content -Content "$Postfix_2" -BackgroundColor $BackgroundColor -ForegroundColor $ForegroundColor -NoNewline
+
+  # Right Boder
+  $BackgroundColor = 'Blue'
+  $ForegroundColor = 'White'
+  Write-Content -Content $Border -BackgroundColor $BackgroundColor -ForegroundColor $ForegroundColor
 
   # Write a blue line
   #Write-Line -Size $LineLength -Content " " -Color Blue
