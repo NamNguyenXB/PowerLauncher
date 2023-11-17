40c03e1ec72bb1af2747b70f096a72effe55611c
Tony Nguyen <TNguyen@paritycorporation.onmicrosoft.com>
2023-11-17 09:28:53 +0700

    Update code

--- PowerLauncher.Script/Modules/PowerLauncher/public/Invoke-Module.ps1
+++ PowerLauncher.Script/Modules/PowerLauncher/public/Invoke-Module.ps1
@@ -1,6 +1,9 @@
 <#
 .SYNOPSIS
-Start a Power Module.
+Invoke a Power Module.
+
+.DESCRIPTION
+Invoke a Power Module.
 
 .PARAMETER Module
 Module to run.
@@ -12,20 +15,29 @@ Run configuration.
 
 #>
 function Invoke-Module {
+  [CmdletBinding()]
   param(
-    [Parameter()]
+    [Parameter(Mandatory = $true)]
+    [ValidateNotNull()]
     $Module,
 
     [Parameter()]
-    $Config
+    [ValidateNotNull()]
+    $Config = @{}
   )
 
-  try {
-    # Validate Module before running
-    if (Confirm-ModuleRun -Module $Module) {
+  begin {}
+
+  process {
+    # Stop processing if the module is disabled.
+    if ($true -ne $Module.Run) {
+      return;
+    }
+
+    try {
       # Write Module Head
       Write-ModuleHead -Module $Module
-
+  
       # Run Module
       try {
         Invoke-ModuleFunction -Module $Module -Config $Config
@@ -37,8 +49,10 @@ function Invoke-Module {
       # Write Module Tail.
       Write-ModuleTail -Module $Module
     }
+    catch {
+      Write-ModuleError -Module $Module -Config $Config -ModuleError $_
+    }
   }
-  catch {
-    Write-ModuleError -Module $Module -Config $Config -ModuleError $_
-  }
+
+  end {}
 }
